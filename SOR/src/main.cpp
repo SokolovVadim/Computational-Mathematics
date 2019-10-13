a3e4a087c6c8687df40ef5a4cca262872029c43f
Vadim <varem99@mail.ru>
2019-10-13 12:18:08 +0300

    tests update

--- SOR/src/main.cpp
+++ SOR/src/main.cpp
@@ -5,11 +5,28 @@ int main(int argc, char* argv[]	)
 {
 	std::cout << "Hello!" << std::endl;
 
-	SOR::Matrix m(argv[1]);
+	SOR::Matrix matrix(argv[1]);
 
-	m.Read();
-	m.Print();
-	// std::size_t dimension = m.GetDimension();
+	matrix.Read();
+	matrix.Print();
 
+	std::size_t dimension = matrix.GetDimension();
 
+	double* vectorB = new double[dimension]; // 1 2 3 4 5
+
+	FillVector(vectorB, dimension);
+	std::cout << "VectorB: " << std::endl;
+	PrintVector(vectorB, dimension);
+
+	double* vectorInit = new double[dimension]; // 1 2 3 4 5
+	
+	std::cout << "VectorInit: " << std::endl;
+	PrintVector(vectorInit, dimension);
+
+	SOR::Successive_Overrelaxation_algorithm(matrix, vectorB, vectorInit, 1e-12, 1.5);
+	std::cout << "Result: " << std::endl;
+	PrintVector(vectorInit, dimension);
+
+	delete[] vectorB;
+	delete[] vectorInit;
 }
